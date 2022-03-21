6f85122550a7f17d9590d88efde00ae1d6adfc4f
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-20 22:01:01 -0300

    Initial style and Building component have a better SEO ☕

---
+++ src/components/Page.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Page({title, description, children}){
+    return(
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content="{description}"/>
+            </Head>
+            {children}
+        </div>
+    )
+}
+
+export default Page
