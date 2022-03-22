a59508674292613bcee4ccb6e97051ff2d203bc1
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-22 14:59:52 -0300

    Estilizando footer e ajustando a dashboard

--- src/components/dash.js
+++ src/components/dash.js
@@ -1,23 +1,37 @@
-import styles from '../../styles/components/dash.module.css'
+import styles from "../../styles/components/dash.module.css";
 
 function Dash() {
   return (
-    <div className={styles.grid}>
+    <div className={styles.dashboard}>
+      <div>
+      <h2>This is the Way</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+        lacinia lectus a dictum blandit. Duis ac erat aliquet, semper tellus
+        quis, efficitur dui. Sed porttitor nisi non mauris maximus porttitor.
+        Proin iaculis mollis nulla imperdiet sollicitudin. Fusce ut placerat
+        augue, id finibus lorem. Pellentesque feugiat suscipit arcu, a egestas
+        massa volutpat ac. In eleifend neque lorem, commodo molestie arcu
+        interdum eget. Proin vitae elit vitae ligula viverra accumsan porta nec
+        nisi. Duis sed posuere sapien.
+      </p>
+      <div/>
+      <div className={styles.grid}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h3>Dev Order 🖺</h3>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
 
-      <a href="https://nextjs.org/docs" className={styles.card}>
-        <h2>Documentation 🖺</h2>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href="https://nextjs.org/docs" className={styles.card}>
-        <h2>Code Radio 📻</h2>
-        <p>
-          Find out your inner peace and bring some emotions to your project.
-        </p>
-      </a>
-
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h3>Code Radio 📻</h3>
+          <p>
+            Find out your inner peace and bring some emotions to your project.
+          </p>
+        </a>
+      </div>
     </div>
+  </div>
   );
 }
 
-export default Dash
+export default Dash;
