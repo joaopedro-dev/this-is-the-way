86aa71b20089a9d2d94e07664147e81d111c779c
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-22 13:13:17 -0300

    testando deploy vercel

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,9 +4,6 @@ function Page({title, description, children}){
     return(
         <div>
             <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anounymos"/>
-            <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet"/>
             <title>{title}</title>
             <meta name="description" content="{description}"/>
             </Head>
