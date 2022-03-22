6db2410315cc6ae56dcc9d53797be50cd493ca26
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-22 16:27:51 -0300

    Refatorando Img

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,15 @@
 import styles from '../../styles/components/header.module.css'
+import Image from 'next/image'
 
 function Header(){
     return (
       <div>
         <div className={styles.container_header}>
           <h1 className={styles.title}>
-            The Developer Journey <img className={styles.logo} src="crystal.png"/>
+            The Developer Journey 
+            <Image src="/crystal.png" alt="Logo" width={80} height={80} />
           </h1>
-          <code className={styles.code}>"may the coffee be with you"</code>
+          <code className={styles.code}>may the coffee be with you</code>
         </div>
       </div>
     )
