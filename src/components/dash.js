71cf675e1cc35d8f81ef877cb546bd99d42f6a01
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-25 23:03:27 -0300

    Testando dark theme

--- src/components/dash.js
+++ src/components/dash.js
@@ -18,12 +18,12 @@ function Dash() {
         <div className={styles.grid}>
           <a className={styles.card}>
             <Image src="/star_wars2.png" alt="Logo" width={100} height={100} />
-            <h3>Code Radio</h3>
+            <h3>The Manifesto</h3>
             <p>Find out and bring some emotions to your project.</p>
           </a>
           <a className={styles.card}>
             <Image src="/star_wars.png" alt="Logo" width={100} height={100} />
-            <h3>Dev Order</h3>
+            <h3>The Journey</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
         </div>
