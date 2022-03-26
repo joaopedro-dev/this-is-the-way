9811512fe7fcd981be4c2b8cbbaa6b5047777a34
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-26 01:56:31 -0300

    Alterando footer

--- src/components/foot.js
+++ src/components/foot.js
@@ -4,13 +4,29 @@ import Image from "next/image";
 function Foot() {
   return (
     <footer className={styles.footer}>
-      <a href="https://github.com/joaopedro-dev"
+      <iframe
+        src="https://open.spotify.com/embed/playlist/6xA9RZ19qAhaeyYeQQNY5v?utm_source=generator&theme=0"
+        height="80"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      ></iframe>
+      <a
+        href="https://github.com/joaopedro-dev"
         target="_blank"
-        rel="noopener noreferrer">
-      <Image src="/qrcode.png" alt="Logo" width={80} height={80} />
+        rel="noopener noreferrer"
+      >
+        <Image src="/Octocat.png" alt="Logo" width={60} height={50} />
+      </a>
+      <a
+        href="https://github.com/joaopedro-dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image src="/linkedin.png" alt="Logo" width={45} height={45} />
       </a>
     </footer>
   );
 }
 
-export default Foot
+export default Foot;
