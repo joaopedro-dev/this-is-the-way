4322c2e69dd49b99e9b23d37285a121420f56949
joaopedro-dev <dev_joaopedro@outlook.com>
2022-03-27 19:38:14 -0300

    Alterando cor principal🦩

--- src/components/foot.js
+++ src/components/foot.js
@@ -9,7 +9,14 @@ function Foot() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image src="/GitHub.png" alt="Logo" width={50} height={50} />
+        <Image src="/GitHub.png" alt="Logo" width={45} height={45} />
+      </a>
+      <a
+        href="https://github.com/joaopedro-dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image src="/spotify_4.png" alt="Logo" width={50} height={50} />
       </a>
     </footer>
   );
