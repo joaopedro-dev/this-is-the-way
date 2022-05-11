33d18eb841fa59ab69d601addecdd1d720d69913
joaopedro-dev <dev_joaopedro@outlook.com>
2022-05-10 21:43:57 -0300

    Alterando page building

--- src/components/building.js
+++ src/components/building.js
@@ -4,9 +4,9 @@ import styles from "../../styles/components/building.module.css";
 function Building(){
     return(
         <div className={styles.container_building}>
-            <h2 className={styles.title_build}>Developing this Page...</h2>
+            <h2 className={styles.title_build}>We still developing this page...</h2>
             <p className={styles.error}>ERROR 420</p>
-            <Image src="/coding.png" alt="developing status" width={128} height={128}/>
+            <Image src="/cannabis.png" alt="developing status" width={128} height={128}/>
         </div>  
     )
 }
