3b423e05abecfc08a03d5ca7361bd97c220c00b6
joaopedro-dev <dev_joaopedro@outlook.com>
2022-05-16 11:41:40 -0300

    Alternando icons da page Journey e alterando nome no header

--- src/components/dash_journey.js
+++ src/components/dash_journey.js
@@ -13,28 +13,28 @@ function DashJourney() {
         <div className={styles.grid}>
           <Link href="">
             <a className={styles.card}>
-              <Image src="/ufo.png" alt="Logo" width={100} height={100} />
+              <Image src="/babysound.png" alt="Logo" width={100} height={100} />
               <h3>Devwan</h3>
               <p>Beginner Level</p>
             </a>
           </Link>
           <Link href="">
             <a className={styles.card}>
-              <Image src="/helmet.png" alt="Logo" width={100} height={100} />
+              <Image src="/monkey.png" alt="Logo" width={100} height={100} />
               <h3>Dev Knight</h3>
               <p>Intermediate Level</p>
             </a>
           </Link>
           <Link href="">
             <a className={styles.card}>
-              <Image src="/kimono.png" alt="Logo" width={100} height={100} />
+              <Image src="/astronaut-1.png" alt="Logo" width={100} height={100} />
               <h3>Dev Master</h3>
               <p>Advanced Level</p>
             </a>
           </Link>
           <Link href="">
             <a className={styles.card}>
-              <Image src="/atom.png" width={100} height={100} />
+              <Image src="/krishna.png" width={100} height={100} />
               <h3>Grand Master</h3>
               <p>God Level</p>
             </a>
