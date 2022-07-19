52cad29ad427951dc92c6b9f585744768186da8e
joaopedro-dev <dev_joaopedro@outlook.com>
2022-07-19 17:37:59 -0300

    Adicionando card Blog

--- src/components/dash.js
+++ src/components/dash.js
@@ -5,42 +5,50 @@ import { motion } from "framer-motion";
 
 function Dash() {
   return (
-    <div className={styles.dashboard}> 
-        <div className={styles.grid}>
-            <motion.div animate="visible" initial="hidden" variants={{
-              hidden:{
-                scale: .8,
-                opacity: 0
+    <div className={styles.dashboard}>
+      <div className={styles.grid}>
+        <motion.div
+          animate="visible"
+          initial="hidden"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
               },
-              visible:{
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  delay: .4
-                }
-              }
-            }}>
+            },
+          }}
+        >
           <Link href="/manifesto">
-              <a className={styles.card}>
-                <Image src="/astronaut.png" alt="Logo" width={90} height={90} />
-                <h3>Manifesto</h3>
-                <p>Here are our purpose and perspective</p>
-              </a>
+            <a className={styles.card}>
+              <Image src="/astronaut.png" alt="Logo" width={90} height={90} />
+              <h3>Manifesto</h3>
+              <p>Here are our purpose and perspective</p>
+            </a>
           </Link>
-            </motion.div>
-          <motion.div animate="visible" initial="hidden" variants={{
-              hidden:{
-                scale: .8,
-                opacity: 0
+        </motion.div>
+        <motion.div
+          animate="visible"
+          initial="hidden"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.2,
               },
-              visible:{
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  delay: .2
-                }
-              }
-            }}>
+            },
+          }}
+        >
           <Link href="/journey">
             <a className={styles.card}>
               <Image src="/saber.png" alt="Logo" width={90} height={90} />
@@ -48,9 +56,34 @@ function Dash() {
               <p>This is the journey to change your destiny forever</p>
             </a>
           </Link>
-            </motion.div>
-        </div>
+        </motion.div>
+        <motion.div
+          animate="visible"
+          initial="hidden"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+              },
+            },
+          }}
+        >
+          <Link href="/manifesto">
+            <a className={styles.card}>
+              <Image src="/book_3.png" alt="Logo" width={90} height={90} />
+              <h3>Blog</h3>
+              <p>Here are our mind</p>
+            </a>
+          </Link>
+        </motion.div>
       </div>
+    </div>
   );
 }
 
